891f4e932a2f716a6a741137d953cad16a3132d9
Kirsten Korevaar <kirsten@eml.cc>
2015-07-14 17:28:38 -0400

    split functions on soup

--- dead.py
+++ dead.py
@@ -8,19 +8,13 @@ def dead(name):
     url = wiki_url.name_to_wiki_url(name)
     r = requests.get(url)
     soup = BeautifulSoup(r.text, "lxml")
-    if soup.find("div", {"class" : "noarticletext"}): 
-        print "I don't know that person"
-    elif soup.find("a", text="disambiguation"):
+    if wiki_url.not_exist(soup):
+        print "I don't know this person"   
+    elif wiki_url.is_ambiguous(soup):
         print "Name is ambiguous, try one of the following:"
-        name_url = "/wiki/" + name.split()[0]
-        for link in soup.find_all('a'):
-            if link.has_attr('href') and link['href'].startswith(name_url):
-                result =  link['href']
-                stripped_result = result[6:].replace('_', ' ')
-                if stripped_result != name:
-                    print stripped_result
-    elif soup.find_all("th", text="Died"):
-	    print "{0} is dead".format(name)
+        wiki_url.print_links(soup, name)
+    elif wiki_url.is_dead(soup):
+        print "{0} is dead".format(name)
     else:
 	    print "{0} is still alive".format(name)
 
--- wiki_url.py
+++ wiki_url.py
@@ -2,3 +2,23 @@ def name_to_wiki_url(name):
     name = name.replace(' ', '_')
     url = 'https://en.wikipedia.org/wiki/'
     return url + name
+
+def not_exist(name):
+	return name.find("div", {"class" : "noarticletext"})
+
+def is_ambiguous(name):
+	return name.find_all("a", text="disambiguation")
+        
+def print_links(doc, name):
+    links = doc.find_all('a')
+    name_url = "/wiki/" + name.split()[0]
+    for link in links:
+        if link.has_attr('href') and link['href'].startswith(name_url):
+            result =  link['href']
+            stripped_result = result[6:].replace('_', ' ')
+            if stripped_result != name:
+                print stripped_result
+
+def is_dead(name):
+	return name.find_all("th", text="Died")
+        
