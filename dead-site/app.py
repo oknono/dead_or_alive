6e94167ab2c8d84d15c972b48e91cb437c6eadea
Kirsten Korevaar <kirsten@eml.cc>
2015-07-27 18:16:06 -0400

    Pass Name as part of URL

--- dead-site/app.py
+++ dead-site/app.py
@@ -1,18 +1,32 @@
 from flask import Flask, render_template
 import requests
+from wiki_request import *
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    result = "Dit is de index"     
-    return render_template('index.html', result=result)
+@app.route('/')
+@app.route('/<name>')
+def index( name = None):
+    if name:
+        name = name.strip().title()
+        r = requests.get(wiki_json(name))
+        try: 
+            result = categories(r)
+            if is_ambiguous(result):
+                message = "{} is an ambiguous name".format(name)
+            elif is_dead(result):
+                message = "{} is dead".format(name)
+            elif was_born(result):
+                message = "{} is still alive".format(name)
+            else:
+                message = "{} doesn't seem to be a person".format(name)
+        except:
+            message = "Wikipedia is not familiar with this name"
+    else:
+        name = ""
+        message = "please append name of person to URL. E.g. \"deadyet.xyz/james brown\""  
 
-@app.route('/<person>')
-def name(person):
-    result = "<person>"
-    render_template('index.html', result=person)
-
+    return render_template('index.html', name=name, message=message)
 
 if __name__ == '__main__':
     app.run()
--- dead-site/wiki_request.py
+++ dead-site/wiki_request.py
@@ -1,25 +1,24 @@
 def wiki_json(name):
-    name = name.strip().title()
     url = 'https://en.wikipedia.org/w/api.php?action=query&titles={0}&continue=&prop=categories&format=json'.format(name)
     return url
 
 def categories(response):
 	return response.json()["query"]["pages"].values()[0]["categories"]
 
-def is_ambiguous(dic):
-    for item in dic:
+def is_ambiguous(d):
+    for item in d:
         if 'disambiguation' in item["title"]:
             return True
     return False
      
-def is_dead(dic):
-    for item in dic:
+def is_dead(d):
+    for item in d:
         if 'deaths' in item["title"]:
             return True
     return False
 
-def was_born(dic):
-    for item in dic:
+def was_born(d):
+    for item in d:
         if 'births' in item["title"]:
             return True
     return False
