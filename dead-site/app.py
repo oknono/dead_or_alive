65291f03d7c607db8b58198dc770b63d2e65b044
Kirsten Korevaar <kirsten@eml.cc>
2016-01-14 16:43:56 +0100

    fix input

--- dead-site/app.py
+++ dead-site/app.py
@@ -1,28 +1,34 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify,request
 import requests
 from wiki_request import *
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-# @app.route('/<name>')
-def index(name = None):
-    if name:
-        result = Wiki_Person(name)
-        if result.is_ambiguous():
-            message = "{} is an ambiguous name".format(name)
-        elif result.is_person():
-            if result.is_dead():
-                message = "{} is dead".format(name)
-            else:
-                message = "{} is still alive".format(name)
-        else:
-            message = "{} is not a person or an unknown person".format(name)
+def index():
+    if request.method == "POST":
+        try:
+            name = request.form['url']
+            print name
+            if name:
+                result = Wiki_Person(name)
+                if result.is_ambiguous():
+                    message = "{} is an ambiguous name".format(name)
+                elif result.is_person():
+                    if result.is_dead():
+                        message = "{} is dead".format(name)
+                    else:
+                        message = "{} is still alive".format(name)
+                else:
+                    message = "{} is not a person or an unknown person".format(name)
+        except:
+            print "Can't retrieve value URL"
+    
     else:
         name = ""
-        message = "please append name of person to URL. E.g. \"deadyet.xyz/james brown\""  
+        message = ""  
 
-    return render_template('index.html', name=name, message=message, )
+    return render_template('index.html', name=name, message=message)
 
 @app.route('/api/<name>') 
 def api(name = None):
