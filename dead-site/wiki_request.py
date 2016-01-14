6f838a6d5684ae47ebf06d3bebd225e7c381ca83
Kirsten <kirsten@eml.cc>
2016-01-14 14:37:01 +0100

    Refactor using OO design

--- dead-site/wiki_request.py
+++ dead-site/wiki_request.py
@@ -1,26 +1,44 @@
-def wiki_json(name):
-    url = 'https://en.wikipedia.org/w/api.php?action=query&titles={0}&continue=&prop=categories&format=json'.format(name)
-    return url
+import requests
 
-def categories(response):
-	return response.json()["query"]["pages"].values()[0]["categories"]
+class Wiki_Person(object):
 
-def is_ambiguous(d):
-    for item in d:
-        if 'disambiguation' in item["title"]:
-            return True
-    return False
-     
-def is_dead(d):
-    for item in d:
-        if 'deaths' in item["title"]:
-            return True
-    return False
-
-def was_born(d):
-    for item in d:
-        if 'births' in item["title"]:
-            return True
-    return False
+    def __init__(self, name):
+        self._name = name.title()
+        self._url = self.wiki_url()
+        self._response = self.wiki_response()
     
-      
+    def wiki_url(self):
+        """Return wiki url for given object"""
+        return'https://en.wikipedia.org/w/api.php?action=query&titles={0}&continue=&prop=categories&format=json'.format(self._name)
+        
+    def wiki_response(self):
+        """Return Json from wikipedia for given object"""
+        return requests.get(self._url).json()
+
+    @property
+    def categories(self):
+        """Return all categories for a given object """
+        try:
+            cat_list = self._response["query"]["pages"].values()[0]["categories"]
+            return [item["title"][9:] for item in cat_list]
+        except KeyError:
+            print "{} is not a wikipedia page".format(self._name)
+    
+    def is_dead(self):
+        for item in self.categories:
+            if 'deaths' in item:
+                return True
+        return False
+
+
+    def is_ambiguous(self):
+        for item in self.categories:
+            if 'disambiguation' in item:
+                return True
+        return False
+
+    def was_born(self):
+        for item in self.categories:
+            if 'births' in item:
+                return True
+        return False
